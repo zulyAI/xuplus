e4681c35aec030295df92006b241a3916b4faa92
dizdev@189.cn <dizdev@189.cn>
2023-09-15 15:43:11 +0800

    First test

--- communication.py
+++ communication.py
@@ -1,10 +1,12 @@
 import zhipuai
 
-zhipuai.api_key = "4aa3832d305a9ea60708bf2c5b1ef2bf.kLu6LT45idWHNJJd"
+# "4aa3832d305a9ea60708bf2c5b1ef2bf.kLu6LT45idWHNJJd" // my id
+#
+zhipuai.api_key = "a6c25810da99d24a71a0893bf5f56e6b.ypC0VAO4DUyuqoGM"
 
 
-def post(post_input: str, role: str, history: str):
-    url = "https://open.bigmodel.cn/api/paas/v3/model-api/chatglm_pro/sse-invoke"
-    prompt = [{"role": role, "content": post_input, "history": history}]
-    return_response = zhipuai.model_api.invoke(model="chatglm_pro", prompt=prompt, top_p=0.7, temperature=0.0)
+def post(post_input: str, role: str, request_id: str):
+    return_response = zhipuai.model_api.invoke(model="chatglm_pro",
+                                               role=role,
+                                               prompt=post_input, top_p=0.7, temperature=0.0, request_id=request_id)
     return return_response
