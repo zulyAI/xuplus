06c6a9b0396d22da08cb3e05764f540ded7430a8
dizdev@189.cn <dizdev@189.cn>
2023-09-16 22:55:47 +0800

    second test

--- communication.py
+++ communication.py
@@ -6,7 +6,11 @@ zhipuai.api_key = "a6c25810da99d24a71a0893bf5f56e6b.ypC0VAO4DUyuqoGM"
 
 
 def post(post_input: str, role: str, request_id: str):
+    # role 需要单独处理
     return_response = zhipuai.model_api.invoke(model="chatglm_pro",
                                                role=role,
-                                               prompt=post_input, top_p=0.7, temperature=0.0, request_id=request_id)
+                                               prompt=post_input,
+                                               top_p=0.7,
+                                               temperature=0.0,
+                                               request_id=request_id)
     return return_response
